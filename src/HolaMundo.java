869094b8a081f23ee88e27d63ab6b25e453dfe56
jlhuacles <joseluishuaclesunocc@gmail.com>
2022-08-07 12:23:22 -0500

    Subo el proyecto inicial

---
+++ src/HolaMundo.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class HolaMundo {
+    public static void main(String[] args){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-MMMM-yyyy");
+
+        LocalDate today = LocalDate.now();
+        /*int day = today.getDayOfMonth();
+        int month = today.getMonthValue();
+        int year = today.getYear(); */
+
+
+        String dateString = today.format(formatter);
+
+
+        System.out.println("Hola Mundo, hoy es " + dateString);
+    }
+}
